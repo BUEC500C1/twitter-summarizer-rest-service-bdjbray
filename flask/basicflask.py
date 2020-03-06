e75e3024fa43ea72d1e13e67d4dade7e815841e1
bdjbray <49975301+bdjbray@users.noreply.github.com>
2020-03-05 20:36:35 -0500

    Add files via upload

--- flask/basicflask.py
+++ flask/basicflask.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for,request
+import cgi
+import cgitb
 
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -8,6 +10,11 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/hello', methods=['POST'])
+def hello():
+    the_name = request.form['user_name']
+    print(the_name)
+    return 'Thanks for usings! <br/> The tweets of %s have been collected! <br/> <a href="/">Back Home</a>' % (the_name)
 
 @app.route("/about")
 def about():
